a2910c3ef032c4237777e5c3eeda9176db1c2aa0
tiobacca <66842904+tiobacca@users.noreply.github.com>
2020-06-22 16:36:49 -0300

    Delete csgo_karambit_marblefade.lua

--- vinland-gmod-servers-master/servers/_shared/pointshop/lua/pointshop/items/csgo_karambit_marblefade.lua
+++
@@ -1,5 +0,0 @@
-ITEM.Name = "Karambit" .. " | " .. "Marble Fade"
-ITEM.Price = 310000
-ITEM.Model = "models/weapons/w_csgo_karambit.mdl"
-ITEM.Skin = 13
-ITEM.WeaponClass = "csgo_karambit_marblefade"
