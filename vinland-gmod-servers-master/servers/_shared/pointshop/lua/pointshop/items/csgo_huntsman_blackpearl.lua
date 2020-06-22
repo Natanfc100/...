9900a922f80f980624dd05171d91ff36eeef6485
tiobacca <66842904+tiobacca@users.noreply.github.com>
2020-06-22 16:22:09 -0300

    Delete csgo_huntsman_blackpearl.lua

--- vinland-gmod-servers-master/servers/_shared/pointshop/lua/pointshop/items/csgo_huntsman_blackpearl.lua
+++
@@ -1,5 +0,0 @@
-ITEM.Name = "Huntsman" .. " | " .. "Blackpearl"
-ITEM.Price = 215000
-ITEM.Model = "models/weapons/w_csgo_tactical.mdl"
-ITEM.WeaponClass = "csgo_huntsman_blackpearl"
-ITEM.PaintMaterial = "models/csgo_knife/huntsman_doppler_blackpearl"
