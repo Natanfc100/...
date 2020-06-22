600af1afaaf54f78b6a9719d0a21ca1982404dc7
tiobacca <66842904+tiobacca@users.noreply.github.com>
2020-06-22 16:32:08 -0300

    Delete csgo_karambit_damascus.lua

--- vinland-gmod-servers-master/servers/_shared/pointshop/lua/pointshop/items/csgo_karambit_damascus.lua
+++
@@ -1,5 +0,0 @@
-ITEM.Name = "Karambit" .. " | " .. "Damascus Steel"
-ITEM.Price = 151000
-ITEM.Model = "models/weapons/w_csgo_karambit.mdl"
-ITEM.Skin = 4
-ITEM.WeaponClass = "csgo_karambit_damascus"
