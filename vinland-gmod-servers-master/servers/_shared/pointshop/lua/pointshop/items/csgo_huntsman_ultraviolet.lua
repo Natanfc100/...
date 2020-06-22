2aa2c8e89fd3e8f29f7f515b7e940400556ca683
tiobacca <66842904+tiobacca@users.noreply.github.com>
2020-06-22 16:28:48 -0300

    Delete csgo_huntsman_ultraviolet.lua

--- vinland-gmod-servers-master/servers/_shared/pointshop/lua/pointshop/items/csgo_huntsman_ultraviolet.lua
+++
@@ -1,5 +0,0 @@
-ITEM.Name = "Huntsman" .. " | " .. "Ultraviolet"
-ITEM.Price = 56000
-ITEM.Model = "models/weapons/w_csgo_tactical.mdl"
-ITEM.Skin = 10
-ITEM.WeaponClass = "csgo_huntsman_ultraviolet"
