9bc798f7cae3eee6849118c3dd248b9784076371
tiobacca <66842904+tiobacca@users.noreply.github.com>
2020-06-22 16:42:55 -0300

    Delete csgo_m9_doppler_phase4.lua

--- vinland-gmod-servers-master/servers/_shared/pointshop/lua/pointshop/items/csgo_m9_doppler_phase4.lua
+++
@@ -1,5 +0,0 @@
-ITEM.Name = "M9 Bayonet" .. " | " .. "Doppler Phase 4"
-ITEM.Price = 200000
-ITEM.Model = "models/weapons/w_csgo_m9.mdl"
-ITEM.WeaponClass = "csgo_m9_doppler_phase4"
-ITEM.PaintMaterial = "models/csgo_knife/m9_doppler_phase4"
