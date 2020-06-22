7df242009e67b3368bbcb1212fa0fdc3ff9907e3
tiobacca <66842904+tiobacca@users.noreply.github.com>
2020-06-22 16:26:09 -0300

    Delete csgo_huntsman_gamma_doppler.lua

--- vinland-gmod-servers-master/servers/_shared/pointshop/lua/pointshop/items/csgo_huntsman_gamma_doppler.lua
+++
@@ -1,5 +0,0 @@
-ITEM.Name = "Huntsman" .. " | " .. "Gamma Doppler"
-ITEM.Price = 120000
-ITEM.Model = "models/weapons/w_csgo_tactical.mdl"
-ITEM.Skin = 18
-ITEM.WeaponClass = "csgo_huntsman_gamma_doppler"
