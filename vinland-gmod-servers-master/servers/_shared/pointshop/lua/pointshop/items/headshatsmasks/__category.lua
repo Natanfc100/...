97fe965c4b616f517e29e525d0c8ae73347928ce
Natan <32806615+Natanfc100@users.noreply.github.com>
2020-06-22 15:17:22 -0300

    Add files via upload

---
+++ vinland-gmod-servers-master/servers/_shared/pointshop/lua/pointshop/items/headshatsmasks/__category.lua
@@ -0,0 +1,12 @@
+CATEGORY.Name = "Chapéus e mascaras"
+CATEGORY.Icon = "emoticon_smile"
+CATEGORY.AllowedEquipped = 2
+CATEGORY.Order = 2
+
+function CATEGORY:OnEquip(ply, modifications, item)
+    ply:PS_AddClientsideModel(item.ID)
+end
+
+function CATEGORY:OnHolster(ply, _, item)
+    ply:PS_RemoveClientsideModel(item.ID)
+end
